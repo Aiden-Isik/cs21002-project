2e4ef003d5c0f4a71e2267743bfc135db56ea3e6
James Dunbar <2624974@dundee.ac.uk>
2025-11-13 19:49:27 +0000

    made the driving simulation work. beginnings of a class to make it parralelisable

--- Entity/obstacle.py
+++ Entity/obstacle.py
@@ -1,19 +1,36 @@
-import pygame
+from math import sin, cos
 
 class Obstacle:
-	@staticmethod
-	def radius() -> float:
-		"""
-		returns the radius of the obstacle in pixels
-		"""
-		return 15.0
+    @staticmethod
+    def radius() -> float:
+        """
+        returns the radius of the obstacle in pixels
+        """
+        return 20.0
 
-	"""
-	An obstacle that has to be avoided by the vehicle that is being driven
-	"""
-	def __init__(self, relative_X: float, relative_Y: float) -> None:
-		"""
-		position of the object is relative to the vehicle
-		"""
-		self.relX = relative_X
-		self.relY = relative_Y
+    """
+    An obstacle that has to be avoided by the vehicle that is being driven
+    """
+    def __init__(self, relative_X: float, relative_Y: float) -> None:
+        """
+        position of the object is relative to the vehicle
+        """
+        self.relX = relative_X
+        self.relY = relative_Y
+
+        self.screenSpaceX = 0.0
+        self.screenSpaceY = 0.0
+
+    def makeScreenSpacePoints(self, screen_X, screen_Y) -> None:
+        """
+        converts the relative positioning of the obstacle into screen space coordinates
+        """
+        self.screenSpaceX = self.relX + (screen_X / 2)
+        self.screenSpaceY = self.relY + (screen_Y / 2)
+
+    def move(self, speed, direction) -> None:
+        """
+        move the object with the speed and direcetion of the car given to it
+        """
+        self.relX -= speed * sin(direction)
+        self.relY += speed * cos(direction)
